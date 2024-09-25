6fe54fafa4a0ae0aa29ebe01e3de301e52b3957b
Logan-FE <logan@doctornow.co.kr>
2024-09-25 14:01:09 +0900

    workingDirectory 설정

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 code-push login "$INPUT_SERVERURL" --accessKey "$INPUT_TOKEN"
+if [ -n "$INPUT_WORKINGDIRECTORY" ]; then
+    cd "$INPUT_WORKINGDIRECTORY"
+fi
 $INPUT_COMMAND --accessKey "$INPUT_TOKEN"
